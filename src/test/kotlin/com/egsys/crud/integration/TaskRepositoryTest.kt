de067a29fee0ab6a861c70340cae22af718033e6
fabiaov <fabioviniciuslds@hotmail.com>
2023-06-28 19:48:29 -0300

    integration test pt 1

---
+++ src/test/kotlin/com/egsys/crud/integration/TaskRepositoryTest.kt
@@ -0,0 +1,34 @@
+package com.egsys.crud.integration
+
+import com.egsys.crud.repository.TaskRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TaskRepositoryTest {
+    @Autowired
+    private lateinit var taskRepository: TaskRepository
+    companion object {
+        @Container
+        private val mysqlContainer = MySQLContainer<Nothing> ("mysql:latest").apply {
+            withDatabaseName("testedb")
+            withUsername("joao")
+            withPassword("12134")
+        }
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
+            registry.add("spring.datasource.password", mysqlContainer::getPassword)
+            registry.add("spring.datasource.username", mysqlContainer::getUsername)
+        }
+    }
+}
